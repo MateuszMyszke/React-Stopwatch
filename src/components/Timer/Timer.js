df9fa4d6619da877221f53ec0e9315a9fe790e52
Mateusz Myszke <myszke93@gmail.com>
2023-03-10 18:34:37 +0100

    Add Tc

--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -1,19 +1,21 @@
 import styles from './Timer.module.scss';
 import Button from '../Button/Button';
 import { useState, useEffect } from 'react';
+import Tc from '../Tc/Tc';
 
 
 const Timer = () => {
   const [time, setTime] = useState(0);
-  const [running, setRunning] = useState('');
+  const [running, setRunning] = useState(false);
   
+
   useEffect(() => {
     let interval;
     if (running) {
       interval = setInterval(() => {
         setTime((prevTime) => prevTime + 10);
       }, 10);
-    } else if (!running) {
+    } else {
       clearInterval(interval);
     }
     return () => clearInterval(interval);
@@ -21,11 +23,7 @@ const Timer = () => {
   
   return (
     <div className={styles.timer}>
-      <div className={styles.numbers}>
-        <span>{("0" + Math.floor((time / 60000) % 60)).slice(-2)}:</span>
-        <span>{("0" + Math.floor((time / 1000) % 60)).slice(-2)}:</span>
-        <span>{("0" + ((time / 10) % 100)).slice(-2)}</span>
-      </div>
+      <Tc time={time}/>
       
         <Button onClick={() => setRunning(true)}>Start</Button>
         <Button onClick={() => setRunning(false)}>Stop</Button>
